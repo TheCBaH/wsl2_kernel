8fb73e5317bf1956cbd82fb68d3f90bdf4bfa530
Vladimir N. Silyaev <vsilyaev@gmail.com>
2021-08-04 23:52:16 -0700

    fixup! Scripts to build WSL2 Linux kernels

--- repo.sh
+++ repo.sh
@@ -7,19 +7,28 @@ repo=wsl2-repo
 _git="git -C $repo.git"
 case "$cmd" in
 init)
-    rm -rf $repo $repo.git;mkdir -p $repo.git
-    $_git init .
-    $_git remote add origin https://github.com/microsoft/WSL2-Linux-Kernel.git
+    if [ ! -d $repo.git ]; then
+        mkdir -p $repo.git
+        $_git init .
+        $_git remote add origin https://github.com/microsoft/WSL2-Linux-Kernel.git
+    fi
     for b in linux-msft-wsl-4.19.y linux-msft-wsl-5.10.y linux-msft-wsl-5.4.y; do
         $_git remote set-branches --add origin $b
     done
+    $_git -c protocol.version=2 fetch --no-tags --depth 1 origin
     ;;
 update)
     ref=$1;shift
     $_git -c protocol.version=2 fetch --no-tags --depth 1 origin
-    rm -rf $repo;cp -rl $repo.git $repo
     _git="git -C $repo"
-    $_git -c protocol.version=2 fetch --no-tags --depth 1 origin $ref:$ref
+    if [ -d $repo ]; then
+        $_git checkout .
+        $_git checkout $($_git rev-parse HEAD)
+        $_git clean -xdf
+    else
+        cp -rl $repo.git $repo
+    fi
+    $_git -c protocol.version=2 fetch --no-tags --depth 1 origin +$ref:$ref
     $_git checkout $ref
     ;;
-done
+esac
