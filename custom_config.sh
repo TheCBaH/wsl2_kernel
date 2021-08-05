65727fa9dd87f16eeb076a4b571653312aa07d49
Vladimir N. Silyaev <vsilyaev@gmail.com>
2021-08-04 22:19:24 -0700

    fixup! Scripts to build WSL2 Linux kernels

---
+++ custom_config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+set -eu
+config=WSL2-Linux-Kernel/Microsoft/config-wsl
+disable () {
+    echo "s/^(CONFIG_$1)=.*/# \1 is not set/"
+}
+
+enable () {
+    echo 's/# (CONFIG_'$1').*/\1=y/'
+}
+
+sed -Ei\
+ -e "$(disable BTRFS_FS)"\
+ -e "$(disable CC_OPTIMIZE_FOR_PERFORMANCE)"\
+ -e "$(disable CEPH)"\
+ -e "$(disable CIFS)"\
+ -e "$(disable CPU_SUP_CENTAUR)"\
+ -e "$(disable HOTPLUG_CPU)"\
+ -e "$(disable IPV6)"\
+ -e "$(disable JBD2)"\
+ -e "$(disable NFSD)"\
+ -e "$(disable NFS_FS)"\
+ -e "$(disable PPP)"\
+ -e "$(disable RANDOMIZE_MEMORY)"\
+ -e "$(disable RETPOLINE)"\
+ -e "$(disable SLHC)"\
+ -e "$(disable WIREGUARD)"\
+ -e "$(disable XFS_FS)"\
+ -e "$(enable CC_OPTIMIZE_FOR_SIZE)"\
+ $config
+
+git -C $(dirname $config) diff
